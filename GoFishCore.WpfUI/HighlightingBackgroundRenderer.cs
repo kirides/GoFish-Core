4724dbf5dd5ab86d70fd57df6d53e38618b3d7d6
Kirides <john.heckendorf@outlook.com>
2019-11-11 14:49:34 +0100

    Optimize Highlighter

--- GoFishCore.WpfUI/HighlightingBackgroundRenderer.cs
+++ GoFishCore.WpfUI/HighlightingBackgroundRenderer.cs
@@ -8,33 +8,29 @@ namespace GoFishCore.WpfUI
 {
     public class HighlightingBackgroundRenderer : IBackgroundRenderer
     {
-        static Pen pen;
+        static Pen pen = new Pen(Brushes.Black, 0.0);
         MainViewModel vm;
 
-        static HighlightingBackgroundRenderer()
-        {
-            pen = new Pen(Brushes.Black, 0.0);
-        }
 
         public HighlightingBackgroundRenderer(MainViewModel host)
         {
             this.vm = host;
         }
 
-        public KnownLayer Layer
-        {
-            get { return KnownLayer.Background; }
-        }
+        public KnownLayer Layer => KnownLayer.Background;
 
         public void Draw(TextView textView, DrawingContext drawingContext)
         {
+            var highlightedLine = vm.HighlightedLine;
+            if (highlightedLine < 0) return;
             foreach (var v in textView.VisualLines)
             {
-                var rc = BackgroundGeometryBuilder.GetRectsFromVisualSegment(textView, v, 0, 1000).First();
                 // NB: This lookup to fetch the doc line number isn't great, we could
                 // probably do it once then just increment.
                 var linenum = v.FirstDocumentLine.LineNumber - 1;
-                if (linenum != vm.HighlightedLine) continue;
+                if (linenum != highlightedLine) continue;
+
+                var rc = BackgroundGeometryBuilder.GetRectsFromVisualSegment(textView, v, 0, 1000).First();
                 var brush = Brushes.Yellow;
                 drawingContext.DrawRectangle(brush, pen, new Rect(0, rc.Top, textView.ActualWidth, rc.Height));
             }
